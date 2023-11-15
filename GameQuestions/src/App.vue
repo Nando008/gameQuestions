ad4f3e74976f240a3e5dca2d5749bfca13d5fe8c
nando008 <josefdorobles0801@gmail.com>
2023-11-15 11:01:42 -0500

    new

---
+++ GameQuestions/src/App.vue
@@ -0,0 +1,30 @@
+
+<script setup>
+import { ref } from 'vue';
+
+
+let num = ref('');
+
+let array = ref([]);
+
+function subir() {
+  array.value.push(num.value)
+}
+
+</script>
+
+
+<template>
+
+  <input @keypress.enter="subir" v-model="num" type="text">
+  <br>
+  <p>{{ array }}</p>
+
+</template>
+
+
+<style scoped>
+
+
+
+</style>
